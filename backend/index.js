9886455081b7a8947ce68b01bdf14029100cda49
aakashDev000 <123662760+aakashDev000@users.noreply.github.com>
2023-07-05 21:32:32 +0530

    react project created

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { getMongodb } = require("./mongodb");
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("server started");
+});
